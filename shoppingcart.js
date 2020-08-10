0142b034e8b393deed8602052416592c36e6b6e3
digitalagestore <digitalagestore@users.noreply.github.com>
2020-08-09 18:08:39 -0700

    update

--- shoppingcart.js
+++ shoppingcart.js
@@ -13,7 +13,7 @@ function displayCart() {
             <table class="carttable">
                 <tr class="cartrow">
                     <th>${item.size}</th>
-                    <th><img src="products/${item.tag}.jpg"></th>
+                    <th><img class="cartimg" src="producticon/${item.tag}.jpg"></th>
                     <th>${item.name}</th>
                     <th>$${item.price}.00</th>
                     <th><input class="cart-qty" type="number" value="${item.inCart}"></th>
@@ -35,24 +35,12 @@ function displayCart() {
 displayCart();
 
 var removeCartItemButtons = document.getElementsByClassName('btn-danger')
-console.log(removeCartItemButtons)
+
 for(var i = 0; i < removeCartItemButtons.length; i++){
     var button = removeCartItemButtons[i]
     button.addEventListener('click', function(event) {
         var buttonClicked = event.target
         buttonClicked.parentElement.parentElement.remove()
+
     })
 }
-
-// function updateCartTotal() {
-//     var cartItemContainer = document.getElementsByClassName('carttable')[0]
-//     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
-//     for(var i = 0; i < cartRows.length; i++){
-//         var cartRow = cartRow[i]
-//         var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-//         var quantityElement = cartRow.getElementsByClassName('cart-qty')[0]
-        
-//         var price = priceElement.innerText
-//         console.log(price)
-//     }
-// }
